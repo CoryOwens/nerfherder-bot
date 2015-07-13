673d21e2b4e6e26c16be2e984962d4f7a450028e
unknown <coryowens@DONKEY.arbfund.com>
2015-07-13 14:03:34 -0600

    Configured setup and loop to use a funtion pointer, effectively allowing for loop() to be redirected as needed. Currently starts by waiting for the button to be pressed, then blinks the LED.

--- botsketch/botsketch.ino
+++ botsketch/botsketch.ino
@@ -1,6 +1,10 @@
 #include <RedBot.h>
 #include <RedBotSoftwareSerial.h>
 
+#define BEEPER_PIN 9
+#define BUTTON_PIN 12
+#define LED_PIN 13
+
 RedBotMotors motors = RedBotMotors();
 RedBotAccel accel = RedBotAccel();
 RedBotSensor leftLine = RedBotSensor(17);
@@ -9,10 +13,41 @@ RedBotSensor rightLine = RedBotSensor(21);
 RedBotBumper leftBumper = RedBotBumper(3);
 RedBotBumper rightBumper = RedBotBumper(11);
 
+void (*func)(); // Function Pointer. Called inside loop(). 
+				// Effectively allows us to point loop() at different function as needed
+
 void setup() {
 	Serial.begin(9600);
+	Serial.println();
 	Serial.println("BEGIN");
+	pinMode(BUTTON_PIN, INPUT_PULLUP);
+	pinMode(LED_PIN, OUTPUT);
+	Serial.println("Waiting For Button");
+	func = &waitForButton; // Initialize func to wait for the button.
 }
 
 void loop() {
+	func();
+}
+
+void waitForButton(){
+	if (digitalRead(BUTTON_PIN) == LOW){
+		Serial.println("Begin Main Program Loop");
+		func = &mainProg;
+	}
+}
+
+void mainProg(){
+	blinker();
+	delay(1000);
+}
+
+void blinker(){
+	static boolean on = false;
+	if (on){
+		digitalWrite(LED_PIN, 1);
+	} else {
+		digitalWrite(LED_PIN, 0);
+	} 
+	on = !on;
 }
